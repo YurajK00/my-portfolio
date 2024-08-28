8cefedbc2274680719d0f1a0f13990b68f4beecd
YurajK00 <urajkharche@gmail.com>
2024-08-28 23:56:31 +0530

    UV 5.0

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -8,9 +8,21 @@
         <div class ="felx flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10" >
 
             <h2 class= "font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Hi I'm <span class = "poppins text-violet-300">Yuraj</span> Kharche <br/> Full Stack
+                Hi I'm <span class = "poppins text-violet-300">Yuraj</span> Kharche <br/> Full Stack 
+                <span class = "poppins text-violet-400">Developer </span>
             </h2>
-        </div>
+            
+            <p class = "text-base sm:text-lg md:text-xl">
+                My <span class = "text-violet-400 ">favorite tech</span> includes Python, SvelteKit Framework, TailwindCSS,  JavaScript (Node.js + Express.js) & PostgreSQL 
+            </p>
+        
+        <a class = "blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"> 
+        <div class= "absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"/>
+
+        <h4 class = "relative z-9"> Get in touch  &rarr;</h4>
+    </a>
+</div>
+
     </section>
 
 
