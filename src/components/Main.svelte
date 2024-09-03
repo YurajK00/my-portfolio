aee9c55be43e9ce182c3af2e45bb2723248b0cbc
YurajK00 <urajkharche@gmail.com>
2024-09-03 09:25:33 +0530

    UV 6.0

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,30 +1,54 @@
 <script> 
-
-
+    // Your script here
 </script>
 
-<main class = "flex felx-col flex-1 p-4">
-    <section id ="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
-        <div class ="felx flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10" >
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
 
-            <h2 class= "font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Hi I'm <span class = "poppins text-violet-300">Yuraj</span> Kharche <br/> Full Stack 
-                <span class = "poppins text-violet-400">Developer </span>
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi I'm <span class="poppins text-violet-300">Yuraj</span> Kharche <br/> Full Stack 
+                <span class="poppins text-violet-400">Developer</span>
             </h2>
             
-            <p class = "text-base sm:text-lg md:text-xl">
-                My <span class = "text-violet-400 ">favorite tech</span> includes Python, SvelteKit Framework, TailwindCSS,  JavaScript (Node.js + Express.js) & PostgreSQL 
+            <p class="text-base sm:text-lg md:text-xl">
+                My <span class="text-violet-400">favorite tech</span> includes Python, SvelteKit Framework, TailwindCSS, JavaScript (Node.js + Express.js) & PostgreSQL 
             </p>
         
-        <a class = "blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"> 
-        <div class= "absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"/>
+            <a class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 
+            cursor-pointer" href = "#" target = "blank">
+                <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"></div>
 
-        <h4 class = "relative z-9"> Get in touch  &rarr;</h4>
-    </a>
+                <h4 class="relative z-9"> Get in touch &rarr;</h4>
+            </a>
+        </div>
+        <div class="relative shadow-2xl grid place-items-center"> 
+            <!-- svelte-ignore a11y-img-redundant-alt -->
+            <img src = {"images/Photocopy.jpg.png"} alt = "Profile image" class = "object-cover z-[2] max-h-[70vh]"/> 
+    </div>
+        </section>
+
+
+
+
+    <section id ="projects" class = "py-20 lg:py-32 flex flex-col gap-24">
+<div class = "flex flex-col gap-2 text-center">
+    <h6 class = "text-lg sm:text-xl md:text-2xl">
+A few of my creative endeavors
+    </h6>
+    <h3 class = "font-semibold text-3xl sm:text-4xl md:text-5xl"> Curious to <span class = "poppins text-violet-400"> see</span> my work?</h3>
+   
 </div>
+<a href = "https://github.com/YurajK00" target= "_blank" class = " mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200 ">
+    <i class = "fa-regular fa-cirlce-play"></i>
+    <p>Go to my github</p>
+ </a>
+
+
+
+
+
+
 
     </section>
-
-
 </main>
-
