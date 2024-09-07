e46dd3af9535a7d28ada76113214622c95199772
YurajK00 <ykha562@aucklanduni.ac.nz>
2024-09-07 19:42:58 +0530

    UV 11.0

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -8,25 +8,29 @@
     ]
 
 </script>
-<header class = {'sticky  z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid' + ( y > 0 ? 'py-4 bg-slate-950 border-violet-950' : 'py-6 bg-transport border-transparent'
+<header class = {'sticky  z-[10] top-0 duration-200 px-10 flex items-center justify-between border border-solid h-16 py-15' + ( y > 0 ? 'bg-slate-950 border-violet-950' : 'bg-transport border-transparent'
 
 ) }>
-    <h1 class = "font-medium"> 
-        <b class = "font-bold poppins text-2xl">Yuraj</b> <span class ="text-xl">Kharche</span>
+    <h1 class = "font-medium px-4"> 
+        <b class = "font-bold poppins text-3xl">Yuraj</b> <span class ="text-2xl">Kharche</span>
     </h1>
-    <div class ="sm:flex ml-auto pr-4 items-center gap-4 hidden">
+    <div class ="sm:flex ml-auto pr-10 items-center gap-4 hidden">
         {#each tabs as tab, index}
-        <a href= {tab.link} class ="duration-200 hover:text-violet-400">
+        <a href= {tab.link} class =" text-2xl duration-200 hover:text-violet-400 poppins">
             <p>{tab.name}</p>
         </a>
         {/each}
     </div>
-    <a href ="https://www.linkedin.com/in/yuraj-kharche-a299633a/" 
+   
+    <a href ="https://drive.google.com/file/d/1oZ6o-DFNZvlwkuXfOK50ZSSK3qF65qmK/view?usp=drive_link" 
     target = "_blank"
-    class ="blueShadow relative overflow-hidden px-5 py-3 mt-2 group rounded-full bg-white text-slate-950">
+    class ="overflow-hidden relative mt-2 px-4 py-2 group rounded-full bg-white text-slate-950">
 
         <div class= "absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"></div>
-        <h4 class = "relative z-9"> Get in touch </h4>
+        <h2 class = " relative z-10 poppins text-xl"> My Resume <sup><span class= "text-xs scale-75 pl-0.5">
+            <i class = "fa-solid fa-arrow-up-right-from-square text-xs scale-[85%]"/>
+          </span></sup> 
+        </h2>
 
     </a>
 </header>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -15,7 +15,7 @@ let benefits = [
 </script>
 
 <main class="flex flex-col flex-1 p-10">
-    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-10 sm:py-14">
         <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
 
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
